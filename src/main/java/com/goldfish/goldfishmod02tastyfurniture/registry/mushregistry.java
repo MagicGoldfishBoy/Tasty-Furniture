5cab7abc4a1f0bdc6feff7ec061cf74cd10a5384
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2024-06-24 11:58:37 -0400

    added chorus mush

--- src/main/java/com/goldfish/goldfishmod02tastyfurniture/registry/mushregistry.java
+++ src/main/java/com/goldfish/goldfishmod02tastyfurniture/registry/mushregistry.java
@@ -75,6 +75,10 @@ public class mushregistry {
     public static final DeferredHolder<Item, Item> CARROT_MUSH = MUSH.register("carrot_mush",
             () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
             .alwaysEdible().nutrition(1).saturationModifier(0.3f).build())));
+
+    public static final DeferredHolder<Item, Item> CHORUS_MUSH = MUSH.register("chorus_mush",
+            () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
+            .alwaysEdible().nutrition(1).saturationModifier(0.3f).build())));
   
             
     }
