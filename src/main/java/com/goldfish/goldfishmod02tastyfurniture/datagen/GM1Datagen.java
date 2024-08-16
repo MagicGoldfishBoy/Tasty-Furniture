65c0d598eca032f72249d120a3d8650c691bc203
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2024-08-16 10:32:30 -0400

    began recipe generator

--- src/main/java/com/goldfish/goldfishmod02tastyfurniture/datagen/GM1Datagen.java
+++ src/main/java/com/goldfish/goldfishmod02tastyfurniture/datagen/GM1Datagen.java
@@ -1,13 +1,17 @@
 package com.goldfish.goldfishmod02tastyfurniture.datagen;
 
 import com.goldfish.goldfishmod02tastyfurniture.TastyFurniture;
+import  com.goldfish.goldfishmod02tastyfurniture.datagen.GM1RecipeProvider;
+import com.goldfish.goldfishmod02tastyfurniture.datagen.GM1RecipeProvider.GM1RecipeProviderConcrete;
 import com.mojang.logging.LogUtils;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
@@ -19,6 +23,7 @@ public class GM1Datagen {
 
     public static void gatherData(GatherDataEvent event) {
         
+        //Blockstates
         try {
         DataGenerator generator = event.getGenerator();
         PackOutput output = generator.getPackOutput();
@@ -31,6 +36,7 @@ public class GM1Datagen {
             LOGGER.error("failed to generate blockstates");
         }
 
+        //Item Models
         try {
         DataGenerator generator = event.getGenerator();
         PackOutput output = generator.getPackOutput();
@@ -42,6 +48,22 @@ public class GM1Datagen {
             
         } catch (Exception e) {
             LOGGER.error("failed to generate item models");
+        }
+
+        //Recipes
+        try {
+            DataGenerator generator = event.getGenerator();
+            PackOutput output = generator.getPackOutput();
+            CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+            
+            generator.addProvider(
+                event.includeServer(),
+                new GM1RecipeProviderConcrete(output, lookupProvider));
+            
+
+            
+        } catch (Exception e) {
+            LOGGER.error("failed to generate recipes");
         }
         
     }
---
+++ src/main/java/com/goldfish/goldfishmod02tastyfurniture/datagen/GM1RecipeProvider.java
@@ -0,0 +1,76 @@
+package com.goldfish.goldfishmod02tastyfurniture.datagen;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.advancements.Criterion;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.HolderLookup.Provider;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraft.world.level.block.Blocks;
+
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.common.crafting.*;
+
+public abstract class GM1RecipeProvider extends RecipeProvider {
+
+    private PackOutput output;
+    private CompletableFuture<Provider> lookupProvider;
+
+    public GM1RecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider) {
+        super(output, lookupProvider);
+        // this.output = output;
+        // this.lookupProvider = lookupProvider;
+        // this.result = null;
+        // this.group = "";
+    }
+
+    // protected final ItemStack result;
+    // protected final Map<String, Criterion<?>> criteria = new LinkedHashMap<>();
+    // @Nullable
+    // protected final String group;
+
+
+    @Override
+    protected void buildRecipes(RecipeOutput output) {
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Items.MUSHROOM_STEW)
+        .requires(Blocks.BROWN_MUSHROOM)
+        .requires(Blocks.RED_MUSHROOM)
+        .requires(Items.BOWL)
+        // Creates the recipe advancement. While not mandated by the consuming background systems,
+        // the recipe builder will crash if you omit this. The first parameter is the advancement name,
+        // and the second one is the condition. Normally, you want to use the has() shortcut for the condition.
+        // Multiple advancement requirements can be added by calling #unlockedBy multiple times.
+        .unlockedBy("has_mushroom_stew", has(Items.MUSHROOM_STEW))
+        .unlockedBy("has_bowl", has(Items.BOWL))
+        .unlockedBy("has_brown_mushroom", has(Blocks.BROWN_MUSHROOM))
+        .unlockedBy("has_red_mushroom", has(Blocks.RED_MUSHROOM))
+        // Stores the recipe in the passed RecipeOutput, to be written to disk.
+        // If you want to add conditions to the recipe, those can be set on the output.
+        .save(output);
+    }
+
+    public static class GM1RecipeProviderConcrete extends GM1RecipeProvider {
+
+        public GM1RecipeProviderConcrete(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider) {
+            super(output, lookupProvider);
+        }
+
+        @Override
+        protected void buildRecipes(RecipeOutput output) {
+            // Implement your recipe generation logic here
+        }
+
+    }
+}
